0e960338912f277b8d934e8fffa4a9bbe76c9057
Jonathan <jonjova1992@hotmail.com>
2022-05-15 19:17:35 -0600

    Guardar

--- my-project/src/components/ListaTareas.vue
+++ my-project/src/components/ListaTareas.vue
@@ -1,17 +1,44 @@
 <template>
-  <form action="">
+  <div>
     <h1>{{ msg }}</h1>
-    <div>
-      <!-- {{this.getDataPagina(1)}} -->
-    </div>
-    <!-- como crear ina tabla con vue -->
+
     <div class="container">
+      <!-- agregar una nueva tarea -->
+
+      <form>
+        <div class="row">
+          <div class="col">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Comentario"
+              v-model="usuarios.body"
+            />
+          </div>
+          <div class="col">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Titulo"
+              v-model="usuarios.title"
+            />
+          </div>
+
+          <button @click="addItem" class="btn btn-dark">
+            <i class="fas fa-plus fa-lg"></i>
+          </button>
+        </div>
+      </form>
+
+      <br />
+      <!-- como crear una tabla con vue -->
       <table class="table table-hover">
         <thead>
           <tr>
             <th scope="col">Id</th>
             <th scope="col">Comentario</th>
             <th scope="col">Titulo</th>
+            <th class="col-2">Edit/Del</th>
           </tr>
         </thead>
         <tbody>
@@ -19,19 +46,38 @@
             <td>{{ item.id }}</td>
             <td>{{ item.body }}</td>
             <td>{{ item.title }}</td>
+            <td>
+              <button @click="item.edit = !item.edit" class="btn btn-info">
+                Editar
+              </button>
+              <button @click="removeItem(index)" class="btn btn-danger">
+                Eliminar
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
       <nav aria-label="Page navigation example">
-      <ul class="pagination justify-content-center">
-        <li class="page-item" v-on:click="getPreviousPage()" ><a class="page-link" href="#">Previous</a></li>
-        <li v-for="(pagina, index) in totalPaginas()" :key="index" @click="getDataPagina(pagina)" class="page-item" v-bind:class="isActive(pagina)"><a class="page-link" href="#">{{pagina}}</a></li>
-        <li class="page-item" v-on:click="getNextPage()"><a class="page-link" href="#">Next</a></li>
-      </ul>
-    </nav>
+        <ul class="pagination justify-content-center">
+          <li class="page-item" v-on:click="getPreviousPage()">
+            <a class="page-link" href="#">Previous</a>
+          </li>
+          <li
+            v-for="(pagina, index) in totalPaginas()"
+            :key="index"
+            @click="getDataPagina(pagina)"
+            class="page-item"
+            v-bind:class="isActive(pagina)"
+          >
+            <a class="page-link" href="#">{{ pagina }}</a>
+          </li>
+          <li class="page-item" v-on:click="getNextPage()">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-    
-  </form>
+  </div>
 </template>
 
 <script>
@@ -42,16 +88,19 @@ import axios from "axios";
 /*en este apartado podemos requerir el diseño de bootraps*/
 // esto esta en la carpeta de src/assets/css
 require("@/assets/css/bootstrap.css");
+require("@/assets/css/fontawesome.min.css");
 
 export default {
   name: "ListaTareas",
   data() {
     return {
+      usuarios: { body: "", title: "", edit: false },
       // Inicializamos usuarios[] para pintar los datos en nuestra tabla
       usuarios: [],
-      elementosPorPagina:5,
-       datosPaginados:[],
-       paginaActual:1,
+      // Variables que se utilizan para la paginación
+      elementosPorPagina: 5,
+      datosPaginados: [],
+      paginaActual: 1,
       // Variables que se usan para entorno del componente v-model
       msg: "Listado de Tareas",
       info: null
@@ -59,13 +108,12 @@ export default {
   },
   /* Aqui estan todos los metodos para crear nuestras peticiones GET/POST/PUT/DELETE */
   methods: {
-   
-     /* La palabra “async” ante una función significa solamente una cosa: que la
+    /* La palabra “async” ante una función significa solamente una cosa: que la
     función siempre devolverá una promesa. */
-    async getDataPagina(noPagina){
-       /*Lo que hace await es esperar a que se resuelva la promesa,
+    async getDataPagina(noPagina) {
+      /*Lo que hace await es esperar a que se resuelva la promesa,
        mientras permite continuar ejecutando otras tareas que puedan realizarse.*/
-       await axios
+      await axios
         .get(
           "https://jsonplaceholder.typicode.com/posts"
         ) /*Consumiendo api rest con get y retornamos un json*/
@@ -80,35 +128,60 @@ export default {
       /*Desde aquí comienza la paginación*/
       this.paginaActual = noPagina;
       this.datosPaginados = [];
-      let ini = (noPagina * this.elementosPorPagina) - this.elementosPorPagina;
-      let fin = (noPagina * this.elementosPorPagina);
-      this.datosPaginados= this.usuarios.slice(ini,fin);
-     
+      let ini = noPagina * this.elementosPorPagina - this.elementosPorPagina;
+      let fin = noPagina * this.elementosPorPagina;
+      this.datosPaginados = this.usuarios.slice(ini, fin);
     },
-    totalPaginas(){
-      return Math.ceil(this.usuarios.length/this.elementosPorPagina);
+    totalPaginas() {
+      return Math.ceil(this.usuarios.length / this.elementosPorPagina);
     },
-    getPreviousPage(){
-        if (this.paginaActual>1) {
-          this.paginaActual--;
-        }
-        this.getDataPagina(this.paginaActual);
+    getPreviousPage() {
+      if (this.paginaActual > 1) {
+        this.paginaActual--;
+      }
+      this.getDataPagina(this.paginaActual);
     },
-    getNextPage(){
-       if (this.paginaActual<this.totalPaginas()) {
-          this.paginaActual++;
-        }
-        this.getDataPagina(this.paginaActual);
+    getNextPage() {
+      if (this.paginaActual < this.totalPaginas()) {
+        this.paginaActual++;
+      }
+      this.getDataPagina(this.paginaActual);
     },
-    isActive(noPagina){
-      return noPagina == this.paginaActual ? 'active':'';
+    isActive(noPagina) {
+      return noPagina == this.paginaActual ? "active" : "";
     },
     /*Aquí finaliza la paginación*/
+    /*Agregar*/
+    addItem() {
+      var datos = {
+        body: this.usuarios.body,
+        title: this.usuarios.title,
+        edit: false
+      };
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts", datos)
+        .then(res => {
+          console.log(res);
+
+          datos = this.datosPaginados.push({
+            body: this.usuarios.body,
+            title: this.usuarios.title,
+            edit: false
+          }); 
+          //  this.usuarios = [];
+        })
+        .catch(e => {
+          console.log("error" + e);
+        });
+  
+      // $("#form-name").focus();
+    }
   },
-  async mounted() {    
+  async mounted() {
     // this.listarTareas();
     this.getDataPagina(1);
-  },
+  }
 };
 </script>
 
